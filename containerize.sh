b60c08b4d1c2f03f8ae0600fa7e9d015c1949dcd
Leonard Lee <leonard.lee@praqma.com>
2017-09-28 22:52:00 +0200

    Update CircleCI configuration.

--- containerize.sh
+++ containerize.sh
@@ -4,4 +4,4 @@
 # conan remove "*" --src --builds --force
 # git clean -dfx && git submodule foreach "git clean -dfx"
 
-docker run --rm --tty --interactive --workdir=/workspace --volume ${PWD}:/workspace praqma/native-conan:0.0.1 ${PWD}/build.sh
+docker run --rm --tty --interactive --workdir=/workspace --volume ${PWD}:/workspace praqma/native-conan:0.0.1 bash
