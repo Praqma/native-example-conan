60f1441b0f7a0f8068e5f6c177467076e387cefe
Leonard Lee <leonard.lee@praqma.com>
2017-09-29 10:26:04 +0200

    Update CircleCI configuration.
    Build directly inside the container under CircleCI.

--- build-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# conan remove "*" --src --builds --force
-# git clean -dfx && git submodule foreach "git clean -dfx"
-# git submodule update --init
-
-docker run --rm --tty --interactive --workdir=/workspace --volume ${PWD}:/workspace praqma/native-conan:latest build.sh
