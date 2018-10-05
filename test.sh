7f8b708647de82139c9be69fa18228e4012c2027
Leonard Lee <leonard.lee@praqma.com>
2018-10-05 16:22:57 +0200

    Update package on the fly.

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@
 set -x
 
 export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
 export WORKSPACE_DIR=/workspace
 export THIRDPARTY_DIR=${WORKSPACE_DIR}/3rdparty
 export EXAMPLE_DIR=${THIRDPARTY_DIR}/conan-gtest-example-master
@@ -19,5 +21,8 @@ if [ "$PWD" != "$WORKSPACE_DIR" ]; then
   exit 1
 fi
 
-cd ${EXAMPLE_DIR} \
-  && conan test_package whatevername/whateverchannel
+cd ${EXAMPLE_DIR}
+sed --in-place=.bak 's/1.0.2l/1.0.2o/g' conanfile.py
+sed --in-place=.bak 's/1.7.0\@lasote/1.8.1\@bincrafters/g' test_package/conanfile.py
+conan test_package . whatevername/whateverchannel
+
